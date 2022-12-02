3c779df1a9457851b9966921731f193f8d3ade68
daniel <danielazocarvelasquezdev@gmail.com>
2022-12-01 23:38:19 -0400

    highlight done

--- src/components/highlight/Highlight.jsx
+++ src/components/highlight/Highlight.jsx
@@ -4,7 +4,7 @@ export const Highlight = () => {
   return (
     <section>
       <div className=' container mx-auto py-20 text-white'>
-        <h2 className=' font-extrabold text-7xl leading-[84px] uppercase w-1/2'>
+        <h2 className=' max-md:w-3/4 max-md:mx-auto font-extrabold max-md:text-center text-3xl md:text-7xl md:leading-[84px] uppercase md:w-1/2'>
           Lorem ipsum dolor
           sit amet,
           consectetur
