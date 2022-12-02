1f7a2d411b5571abb5b8425382e8afa31d24e6b3
daniel <danielazocarvelasquezdev@gmail.com>
2022-12-02 00:38:11 -0400

    fix hero image

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -2,14 +2,14 @@ import { IoAdd } from 'react-icons/io5'
 
 export const Faq = () => {
   return (
-    <section className=" bg-deepViolet text-white pt-56 pb-28">
-      <div className="container mx-auto flex">
-        <div className=" w-1/2">
+    <section className=" bg-deepViolet text-white pt-24 md:pt-56 pb-28">
+      <div className="container mx-auto md:flex">
+        <div className=" md:w-1/2 max-md:text-center max-md:pb-8">
           <h2 className=" font-exo2 font-extrabold text-9xl mb-9">F.A.Q.</h2>
-          <p className="w-2/3 font-nunito uppercase">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          <p className="md:w-2/3 font-nunito uppercase">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </div>
-        <div className="w-1/2">
+        <div className="md:w-1/2">
           <div className='w-full mb-16'>
             <div className=' w-11/12 flex items-center justify-between font-exo2 font-extrabold text-3xl mb-6'>
               <h4>Lorem ipsum dolor sit amet?</h4>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,12 +6,12 @@ export const Hero = () => {
   return (
     <section className='hero bg-deepViolet'>
       <div className=' container mx-auto h-[650px] md:h-[900px] flex flex-col items-center justify-end text-center text-white relative'>
-        <div className=' font-bold font-exo2 absolute top-32'>
+        <div className=' font-bold font-exo2 absolute max-sm:top-24 top-32'>
           <h2 className=' text-7xl md:text-8xl uppercase drop-shadow-xl'>the</h2>
           <h1 className=' text-8xl md:text-9xl uppercase drop-shadow-xl'>event</h1>
         </div>
-        <div className='w-full h-[675px] flex items-stretch z-10 box-border max-md:relative'>
-          <img className='max-sm:min-w-[600px] max-sm:-right-16 w-full  max-md:absolute max-md:bottom-0' src={nftHero} alt="" />
+        <div className='w-full h-3/4 z-10 box-border md:pr-20 max-md:relative overflow-hidden'>
+          <img className='max-sm:min-w-[680px] md:max-w-[960px] absolute left-2/4 -translate-x-[55%] bottom-0 ' src={nftHero} alt="" />
         </div>
       </div>
     </section>
