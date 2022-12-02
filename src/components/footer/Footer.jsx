f9edec3fc7f0ef0a67ee1a003a768b9e65c9126a
daniel <danielazocarvelasquezdev@gmail.com>
2022-12-01 23:33:23 -0400

    footer mobile

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,31 +4,31 @@ import { SiDiscord, SiTwitter, SiInstagram } from 'react-icons/si'
 
 export const Footer = () => {
   return (
-    <footer className=' h-[400px] text-white'>
-      <div className="container mx-auto h-full flex pt-24">
-        <div className='md:w-1/2 flex items-center justify-center '>
-          <div className=' w-2/4'>
+    <footer className=' md:h-[400px] text-white'>
+      <div className="container mx-auto h-full md:flex pt-24">
+        <div className=' md:w-1/3 lg:w-1/2 flex items-center justify-center max-md:pb-8'>
+          <div className='w-2/4 md:w-2/3 lg:w-2/4'>
             <img src={logo} alt="" />
           </div>
         </div>
-        <div className='md:w-1/2 flex flex-col justify-between pb-3'>
-          <div className='flex justify-between'>
+        <div className='md:w-2/3 lg:w-1/2 flex flex-col justify-between pb-3'>
+          <div className='md:flex justify-between max-md:pb-8'>
             <div className=' grid justify-items-center'>
-              <h4 className=' font-exo2 font-bold text-4xl uppercase pb-5'>twitter</h4>
+              <h4 className=' font-exo2 font-bold text-4xl md:text-3xl lg:text-4xl uppercase pb-5'>twitter</h4>
               <SiTwitter className=' text-6xl' />
             </div>
             <div className=' grid justify-items-center'>
-              <h4 className=' font-exo2 font-bold text-4xl uppercase pb-5'>discrod</h4>
+              <h4 className=' font-exo2 font-bold text-4xl md:text-3xl lg:text-4xl uppercase pb-5'>discord</h4>
               <SiDiscord className=' text-6xl' />
             </div>
             <div className=' grid justify-items-center'>
-              <h4 className=' font-exo2 font-bold text-4xl uppercase pb-5'>instagram</h4>
+              <h4 className=' font-exo2 font-bold text-4xl md:text-3xl lg:text-4xl uppercase pb-5'>instagram</h4>
               <SiInstagram className=' text-6xl' />
             </div>
           </div>
-          <div>
+          <div className='max-md:text-center'>
             <h3 className=' font-exo2 font-extrabold text-3xl uppercase'>Terminos y condiciones</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <p className=' font-nunito '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
           </div>
         </div>
       </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import metamask from '../../assets/metamask-icon.svg'
 
 export const Navbar = () => {
   return (
-    <nav className=" w-full  h-[70px] text-lg fixed top-0 left-0 right-0  backdrop-blur-lg z-20">
+    <nav className=" w-full h-[70px] max-md:px-4 text-lg fixed top-0 left-0 right-0  backdrop-blur-lg z-20">
       <div className=" container mx-auto h-full flex justify-between items-center">
         <h2 className=" font-exo2 font-bold text-2xl uppercase  text-gray-200 hover:text-cta cursor-pointer">The Event</h2>
         <div className='flex gap-3 text-cta hover:text-gray-200 cursor-pointer'>
