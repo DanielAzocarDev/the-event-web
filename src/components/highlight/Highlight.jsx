c3ebe7a8fb29a0396ae535caf1319e01d87ba5be
daniel <danielazocarvelasquezdev@gmail.com>
2022-12-16 12:03:26 -0400

    test

--- src/components/highlight/Highlight.jsx
+++ src/components/highlight/Highlight.jsx
@@ -1,18 +1,17 @@
-import { motion, useInView } from "framer-motion"
 import { useRef } from "react"
+import { motion, useInView } from "framer-motion"
 
 export const Highlight = () => {
   const ref = useRef(null)
   const isInView = useInView(ref)
 
-  console.log(isInView)
   return (
     <section >
       <div className=' container mx-auto py-20 text-white'>
         <motion.h2 ref={ref} style={{
           transform: isInView ? "none" : "translateX(-100px)",
           opacity: isInView ? 1 : 0,
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.6s"
         }} className=' max-md:w-3/4 max-md:mx-auto font-extrabold max-md:text-center text-3xl md:text-7xl md:leading-[84px] uppercase md:w-1/2'>
           Lorem ipsum dolor
           sit amet,
